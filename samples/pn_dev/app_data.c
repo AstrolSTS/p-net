bf2b14aab209954b34751c176191b37399c3bd2c
Stefan Siegrist <sts@astrol.ch>
2024-05-07 14:31:07 +0200

    wip ubus

--- samples/pn_dev/app_data.c
+++ samples/pn_dev/app_data.c
@@ -25,6 +25,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define APP_DATA_DEFAULT_OUTPUT_DATA 0
 
