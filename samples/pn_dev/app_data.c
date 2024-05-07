365059cd0ecb7fc81ce9fb216cf0d5fed5d16e89
Stefan Siegrist <sts@astrol.ch>
2024-05-07 09:00:08 +0200

    wip gsd

--- samples/pn_dev/app_data.c
+++ samples/pn_dev/app_data.c
@@ -139,7 +139,7 @@ uint8_t * app_data_get_input_data (
          inputdata[0] &= 0x7F;
       }
       */
-     
+
       *size = APP_GSDML_OUTPUT_DATA_DIGITAL_SIZE;
       *iops = PNET_IOXS_GOOD;
       return outputdata;
@@ -198,7 +198,7 @@ int app_data_set_output_data (
          memcpy (outputdata, data, size);
 
          /* Most significant bit: LED */
-         //led_state = (outputdata[0] & 0x80) > 0;
+         led_state = 0;//(outputdata[0] & 0x80) > 0;
          //app_handle_data_led_state (led_state);
 
          return 0;
