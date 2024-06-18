b290957bd67c726c3a5e3deee6c6d316a32203d7
Stefan Siegrist <sts@astrol.ch>
2024-06-18 11:31:21 +0200

    remove pn_com_supervisor

--- samples/pn_dev/app_data.c
+++ samples/pn_dev/app_data.c
@@ -79,7 +79,7 @@ CC_STATIC_ASSERT (sizeof (app_echo_data_t) == APP_GSDML_OUTPUT_DATA_ECHO_SIZE);
 static struct ubus_context *ctx;
 static struct blob_buf b;
 static bool initDone = false;
-static uint32_t pnComSupervisor = 0;
+//static uint32_t pnComSupervisor = 0;
 
 typedef struct {
    uint8_t status0;
