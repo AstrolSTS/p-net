c50992a8e31c7f97e4ba939c5b88f1e722cbd7d3
Stefan Siegrist <sts@astrol.ch>
2024-06-19 14:03:03 +0200

    resync

--- samples/pn_dev/app_data.c
+++ samples/pn_dev/app_data.c
@@ -80,7 +80,7 @@ CC_STATIC_ASSERT (sizeof (app_echo_data_t) == APP_GSDML_OUTPUT_DATA_ECHO_SIZE);
 static struct ubus_context *ctx;
 static struct blob_buf b;
 static bool initDone = false;
-static int16_t reSyncIndex = 0;
+//static int16_t reSyncIndex = 0;
 clock_t t_start, t_end;
 double cpu_time_used;
 //static uint32_t pnComSupervisor = 0;
